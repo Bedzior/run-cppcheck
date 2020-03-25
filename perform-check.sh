636816c060bc81eb9c16200612803f2847164148
Rafal Bedzkowski <ravcik@gmail.com>
2020-03-25 22:30:44 +0100

    First attempt

---
+++ perform-check.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+REPORT_FILE=report.xml
+
+cppcheck src \
+    --enable=$INPUT_ENABLEDCHECKS \
+    ${INPUT_ENABLEDINCONCLUSIVE:+--inconclusive} \
+    --output-file=REPORT_FILE \
+    -j `nproc` \
+    --xml $INPUT_EXCLUDES $INPUT_INCLUDES
+
+cppcheck-htmlreport \
+    --file=$REPORT_FILE \
+    --title=$INPUT_REPORTNAME \
+    --report-dir=output
