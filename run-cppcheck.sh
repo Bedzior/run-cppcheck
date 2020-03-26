cb43de9cef96656a82268d622e098aa5a2c71eca
Rafal Bedzkowski <ravcik@gmail.com>
2020-03-26 22:03:21 +0100

    Fixing script

--- run-cppcheck.sh
+++ run-cppcheck.sh
@@ -1,35 +1,32 @@
 #!/bin/sh
 
-
-function split_join_lines {
-    local IFS=$'\n'
-    local lines=($1)
-    local i
-    local prefix=$2
-    local output=''
-    for (( i=0; i<${#lines[@]}; i++ )) ; do
-        output="$output $prefix${lines[$i]}"
+split_join_lines() {
+    prefix=$2
+    output=''
+    echo "$1" | while read line
+    do
+        output="$output $prefix$line"
     done
     echo output
 }
 
-if [[ $INPUT_GENERATEREPORT == 'true']]; then
+if [ "$INPUT_GENERATEREPORT" = 'true' ]; then
     GENERATEREPORT='yep'
     REPORT_FILE=report.xml
 fi
 
 cppcheck src \
-    --enable=$INPUT_ENABLEDCHECKS \
+    --enable="$INPUT_ENABLEDCHECKS" \
     ${INPUT_ENABLEDINCONCLUSIVE:+--inconclusive} \
     ${GENERATEREPORT:+--output-file=$REPORT_FILE} \
-    -j `nproc` \
+    -j "$(nproc)" \
     --xml \
-    `split_join_lines $INPUT_INCLUDES '-I' \
-    `split_join_lines $INPUT_EXCLUDES '-i'
+    "$(split_join_lines "$INPUT_INCLUDES" '-I')" \
+    "$(split_join_lines "$INPUT_EXCLUDES" '-i')"
 
-if [[ $GENERATEREPORT ]]; then
+if [ $GENERATEREPORT ]; then
     cppcheck-htmlreport \
         --file=$REPORT_FILE \
-        --title=$INPUT_REPORTNAME \
+        --title="$INPUT_REPORTNAME" \
         --report-dir=output
 fi
