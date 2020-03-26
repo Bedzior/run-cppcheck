2a0c5dc7227b0690c37239fd9af3ac21fffdbe17
Rafal Bedzkowski <ravcik@gmail.com>
2020-03-26 23:26:09 +0100

    Fixed string splitting

--- run-cppcheck.sh
+++ run-cppcheck.sh
@@ -1,19 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 set -x
 
 split_join_lines() {
-    prefix=$2
     output=''
-    while read line; do (( output="${output:+ }$prefix$line" )); done < <(echo "$1")
+    readarray -t lines <<<"$1"
+    printf -v output "$2%s " "${lines[@]}"
     echo "$output"
 }
 
-if [ "$INPUT_GENERATEREPORT" = 'true' ]; then
+if [[ "$INPUT_GENERATEREPORT" == 'true' ]]; then
     GENERATEREPORT='yep'
     REPORT_FILE=report.xml
 fi
 
-if [ "$INPUT_ENABLEDINCONCLUSIVE" = 'true' ]; then
+if [[ "$INPUT_ENABLEDINCONCLUSIVE" == 'true' ]]; then
     ENABLEINCONCLUSIVE='yep'
 fi
 
@@ -23,8 +23,8 @@ cppcheck src \
     ${GENERATEREPORT:+--output-file=$REPORT_FILE} \
     -j "$(nproc)" \
     --xml \
-    "$(split_join_lines "$INPUT_INCLUDES" '-I')" \
-    "$(split_join_lines "$INPUT_EXCLUDES" '-i')"
+    "$(split_join_lines "$INPUT_INCLUDES" '\055I')" \
+    "$(split_join_lines "$INPUT_EXCLUDES" '\055i')"
 
 if [ $GENERATEREPORT ]; then
     cppcheck-htmlreport \
