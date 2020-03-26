29bdc7ce6ebfbdddab4ae284de2817b4ba9cbf4f
Rafal Bedzkowski <ravcik@gmail.com>
2020-03-26 21:47:43 +0100

    Upgrading

--- run-cppcheck.sh
+++ run-cppcheck.sh
@@ -1,16 +1,35 @@
 #!/bin/sh
 
-REPORT_FILE=report.xml
+
+function split_join_lines {
+    local IFS=$'\n'
+    local lines=($1)
+    local i
+    local prefix=$2
+    local output=''
+    for (( i=0; i<${#lines[@]}; i++ )) ; do
+        output="$output $prefix${lines[$i]}"
+    done
+    echo output
+}
+
+if [[ $INPUT_GENERATEREPORT == 'true']]; then
+    GENERATEREPORT='yep'
+    REPORT_FILE=report.xml
+fi
 
 cppcheck src \
     --enable=$INPUT_ENABLEDCHECKS \
     ${INPUT_ENABLEDINCONCLUSIVE:+--inconclusive} \
-    --output-file=$REPORT_FILE \
+    ${GENERATEREPORT:+--output-file=$REPORT_FILE} \
     -j `nproc` \
     --xml \
-    $INPUT_EXCLUDES $INPUT_INCLUDES
+    `split_join_lines $INPUT_INCLUDES '-I' \
+    `split_join_lines $INPUT_EXCLUDES '-i'
 
-cppcheck-htmlreport \
-    --file=$REPORT_FILE \
-    --title=$INPUT_REPORTNAME \
-    --report-dir=output
+if [[ $GENERATEREPORT ]]; then
+    cppcheck-htmlreport \
+        --file=$REPORT_FILE \
+        --title=$INPUT_REPORTNAME \
+        --report-dir=output
+fi
